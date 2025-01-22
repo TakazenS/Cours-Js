c23c348fcbea66950ffec7952ba8ea7422e9adcd
Loris Moine <loris.moine1402@gmail.com>
2025-01-22 13:05:00 +0100

    Added Commentary

--- Code/Errors-Handlings.js
+++ Code/Errors-Handlings.js
@@ -1,8 +1,8 @@
 //Errors
-try {
+try {             // block of code to try
     undefinedFunction();
 }
-catch(err) {
+catch(err) {      // block to handle errors
     console.log(err.message);
 }
 
@@ -11,15 +11,16 @@ catch(err) {
 var x = document.getElementById("mynum").value;
 
 try {
-    if(x == "") throw "empty";
+    if(x == "") throw "empty";          // error cases
     if(isNaN(x)) throw "not a number";
     x = Number(x);
     if(x > 10) throw "too high";
 }
-catch(err) {
-    document.write("Input is " + err);
-    console.error(err);
+catch(err) {                            // if there's an error
+    document.write("Input is " + err);  // output error
+    console.error(err);             // write the error in console
 }
 finally {
+    // executed regardless of the try / catch result
     document.write("</br />Done");
 }
