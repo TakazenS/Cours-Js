86ebd4354292cd4a9ffdc704e55ed7300c75dc2a
Loris Moine <loris.moine1402@gmail.com>
2025-01-22 13:09:23 +0100

    Added Commentary

--- Code/Javascript-JSON.js
+++ Code/Javascript-JSON.js
@@ -1,26 +1,26 @@
 //Javascript JSON
-var str = '{"names" : [' +
+var str = '{"names" : [' +                          // create JSON object
     '{"first" :  "Hakuna", "last" : "Matata"},' +
     '{"first" :  "Jane", "last" : "Doe"},' +
     '{"first" :  "Air", "last" : "Jordan"}]}';
 
-myObj = JSON.parse(str);
-document.write(myObj.name[1].first);
-var myJSON = JSON.stringify(myObj);
-localStorage.setItem("testJSON", myJSON);
-text = localStorage.getItem("testJSON");
+myObj = JSON.parse(str);                            // parse
+document.write(myObj.name[1].first);                // access
+var myJSON = JSON.stringify(myObj);                 // stringify
+localStorage.setItem("testJSON", myJSON);           // storying data
+text = localStorage.getItem("testJSON");            // retrieving data
 
 
 //Javascript object
-var student = {
-    firstName: "Jane",
+var student = {                 // object name
+    firstName: "Jane",          // list of proprieties and values
     lastName: "Doe",
     age: 18,
     height: 170,
-    fullName: function() {
+    fullName: function() {      // object function
         return this.firstName + " " + this.lastName;
     }
 };
-student.age = 19;
-student["height"]++;
-name = student.fullName();
+student.age = 19;               // setting value
+student["height"]++;            // incrementing
+Name = student.fullName();      // call object function
